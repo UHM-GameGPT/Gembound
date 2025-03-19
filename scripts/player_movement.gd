f098f2f8708366e34e8fc5323e336ba0565cbcf6
Keiko Raiola <keikotr21@gmail.com>
2025-03-18 22:01:46 -1000

    #3 Added tutorial scene changes and platform collisions
    - Added transition between two different tutorial scenes
    - Added platforms and one way collisions

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+class_name Player
+
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
@@ -40,36 +42,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
-"""
-extends CharacterBody2D
-
-var max_speed = 100;
-var last_direction = Vector2(1,0)
-
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
-func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("left", "right", "up", "down")
 	
-	velocity = direction * max_speed
-	move_and_slide()
-	
-	if direction.length() > 0:
-		last_direction = direction
-		play_walk_animation(direction)
-	else:
-		play_idle_animation(last_direction)
-
-func play_walk_animation(direction):
-	if direction.x > 0:
-		animated_sprite.play("walk_right")
-	elif direction.x < 0:
-		animated_sprite.play("walk_left")
-
-func play_idle_animation(direction):
-	if direction.x > 0:
-		animated_sprite.play("idle_right")
-	elif direction.x < 0:
-		animated_sprite.play("idle_left")
-"""
+func _input(event : InputEvent):
+	if(event.is_action_pressed("down")):
+		position.y += 1
