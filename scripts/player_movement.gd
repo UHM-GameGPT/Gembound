68b2a092067c92a14d748fb1139184249054f41e
Iverson Yago <iversony@hawaii.edu>
2025-04-03 20:26:47 -1000

    Tutorial Enemy Level
    - Added death animation for player
    - Added spikes and enemy slime that kills players when touched

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -8,8 +8,10 @@ const JUMP_VELOCITY = -300.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jump: AudioStreamPlayer = $Jump
 @onready var jump_land: AudioStreamPlayer = $JumpLand
+@onready var spawn_point = get_parent().get_node("PlayerSpawn")
 
 var landing: bool = false
+var is_dead: bool = false
 
 func _ready():
 	NavigationManager.on_trigger_player_spawn.connect(_on_spawn)
@@ -67,6 +69,22 @@ func _physics_process(delta: float) -> void:
 	elif not is_on_floor():
 		landing = true
 
+func die():
+	if is_dead: return
+	is_dead = true
+
+	animated_sprite.play("death")
+	set_physics_process(false)
+	$CollisionShape2D.call_deferred("set_disabled", false)
+
+	await get_tree().create_timer(1).timeout
+
+	global_position = spawn_point.global_position
+	animated_sprite.play("idle_right")
+
+	is_dead = false
+	set_physics_process(true)
+	$CollisionShape2D.call_deferred("set_disabled", false)
 	
 func _input(event : InputEvent):
 	if(event.is_action_pressed("down")):
