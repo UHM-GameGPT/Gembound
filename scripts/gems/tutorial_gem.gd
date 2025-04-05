b4dd86b4816d0ef0a795bf19c8359d949d8ea8d1
Keiko Raiola <143131373+keikotr@users.noreply.github.com>
2025-04-04 18:11:19 -1000

    #3 Added gem collection, dash ability, and more sound fx
    - Added the tutorial gem collection scene
    - Added gem placeholder + sound fx for when it is collected
    - Implemented dashing mechanic
    - Added player dashing animations
    - Updated sound fx for when the player is on the grass

---
+++ scripts/gems/tutorial_gem.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+@onready var gem: AnimatedSprite2D = $Gem
+@onready var gem_collected: AudioStreamPlayer = $Collected
+
+@export var gem_name: String = "gem_tutorial"
+
+func _ready() -> void:
+	if GemManager.collected_gems.has(gem_name):
+		queue_free()
+	else:
+		gem.play("idle")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		# Collect gem and notify GemManager
+		GemManager.collect_gem(gem_name)
+		gem.visible = false
+		gem_collected.play()
+		await gem_collected.finished
+		PlayerState.dash_unlocked = true
+		body.can_dash = true
+		queue_free()
