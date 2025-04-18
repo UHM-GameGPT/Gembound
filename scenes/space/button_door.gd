4b3bf1b687559221264a1b7433375e6589fc8133
Keiko Raiola <keikotr@hawaii.edu>
2025-04-17 16:56:25 -1000

    #12 Improved door opening mechanism
    - Added short door opening animation
    - Added doors for space level 1 and 2

--- scenes/space/button_door.gd
+++ scenes/space/button_door.gd
@@ -1,9 +1,14 @@
-extends Node2D
+extends StaticBody2D
+
+@onready var anim_sprite = $AnimatedSprite2D
+@onready var collider = $CollisionShape2D
 
 func open():
-	$CollisionShape2D.disabled = true
-	$AnimatedSprite2D.play("open")
+	if anim_sprite.animation != "disappear":
+		anim_sprite.play("disappear")
+		collider.call_deferred("set_disabled", true)
 
 func close():
-	$CollisionShape2D.disabled = false
-	$AnimatedSprite2D.play("close")
+	if anim_sprite.animation != "reappear":
+		anim_sprite.play("reappear")
+		collider.call_deferred("set_disabled", false)
