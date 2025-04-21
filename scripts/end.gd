86720c232342e0fe068f20271804d80c6ac6b6dc
susankpma <susanm@hawaii.edu>
2025-04-20 23:47:18 -1000

    Changed end message. Added two characters. Different gems animated in a path 2d

--- scripts/end.gd
+++ scripts/end.gd
@@ -3,16 +3,13 @@ extends TextureRect
 @onready var animated_sprite: AnimatedSprite2D = $Player/AnimatedSprite2D
 @onready var timer: Timer = $Timer
 
+var angle:float = 0.0
+var speed:float = 1.5
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animated_sprite.play("jumping")
 	timer.start()
 	AudioManager.play_music_tutorial()
 
-func _process(delta: float) -> void:
-	pass
-	#animated_sprite.play("jumping")
-
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
-	pass # Replace with function body.
