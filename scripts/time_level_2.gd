e84a9ede1e96813b972469c584bd3cc3950b0ac2
susankpma <susanm@hawaii.edu>
2025-04-27 21:37:14 -1000

    testing gem counter

--- scripts/time_level_2.gd
+++ scripts/time_level_2.gd
@@ -1,11 +1,19 @@
 extends TextureRect
+@onready var gemlabel: Label = $gemlabel
+@onready var time_gem_1 = $time_gem1
 
 func _ready():
 	#coconutbutton.disabled = true
 	AudioManager.play_music_tutorial()
 	if NavigationManager.spawn_door_tag !=null:
 		_on_level_spawn(NavigationManager.spawn_door_tag)
+
+	print(GemManager.collected_gems.has("gem_time1"))	
+	if GemManager.collected_gems.has("gem_time1"):
+		time_gem_1.visible = true
 		
+		gemlabel.text = str(GemManager.collected_gems.size() - 1) + "/6"
+	
 func _on_level_spawn(destination_tag: String):
 	var door_path = "Doors/Door_" + destination_tag
 	var door = get_node(door_path) as Door
