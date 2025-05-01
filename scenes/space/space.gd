83f0d2d31d036f8062c518cbf2e3282312aba4a5
susankpma <susanm@hawaii.edu>
2025-05-01 01:47:10 -1000

    updates

--- scenes/space/space.gd
+++ scenes/space/space.gd
@@ -6,7 +6,7 @@ func _ready():
 	if NavigationManager.spawn_door_tag !=null:
 		_on_level_spawn(NavigationManager.spawn_door_tag)
 	
-	gem_counter.countgems()
+	# gem_counter.countgems()
 		
 func _on_level_spawn(destination_tag: String):
 	var door_path = "Doors/Door_" + destination_tag
