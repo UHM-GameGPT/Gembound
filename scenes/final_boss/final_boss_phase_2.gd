52042f1aaa753a8a3f85686bd3c1700580665d3e
Iverson Yago <iversony@hawaii.edu>
2025-05-02 00:44:31 -1000

    Small changes
    - Made it so it wasn't white screened

--- scenes/final_boss/final_boss_phase_2.gd
+++ scenes/final_boss/final_boss_phase_2.gd
@@ -2,8 +2,7 @@ extends TextureRect
 
 func _ready():
 	AudioManager.play_music_finalboss()
-	await get_tree().create_timer(1.0).timeout
-	$Text.visible = false  
+
 	if not Global.has_faded_from_white_in_phase2:
 		$WhiteFade.modulate = Color(1, 1, 1, 1)
 		var tween = create_tween()
@@ -11,3 +10,5 @@ func _ready():
 		Global.has_faded_from_white_in_phase2 = true
 	else:
 		$WhiteFade.modulate = Color(1, 1, 1, 0)  # Skip fade; start clear
+	await get_tree().create_timer(4.0).timeout
+	$Text.visible = false  
